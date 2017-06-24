c3201b2cf1d44678b088f686af2d6305a9ac0757
Christian De Kievit <cdekievit@gmail.com>
2017-06-24 15:18:01 +1000

    Removed service worker registration for now.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import registerServiceWorker from './registerServiceWorker';
 
 import {Provider} from 'react-redux';
 import configureStore from './store/configure-store';
@@ -18,10 +17,8 @@ store.dispatch(getLatestEventDetails());
 
 store.dispatch(loadBasicContent());
 
-
 ReactDOM.render(
   <Provider store={store}>
     <AppContainer />
   </Provider>
   , document.getElementById('root'));
-registerServiceWorker();
