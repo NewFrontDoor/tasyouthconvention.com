bc48b372723f1b2a3c92d72dde31b2266e1e582f
Bratalix <bratalix@gmail.com>
2019-07-25 09:55:44 +1000

    Email fix

--- src/components/pages/registration/success-section.js
+++ src/components/pages/registration/success-section.js
@@ -10,7 +10,7 @@ export default class SuccessSection extends React.Component {
     return (
         <div>
           <div className="alert alert-success">Success! you have successfully registered for TYC.</div>
-          <p>You should receive an email confirming your registration and letting you know all the details about TYC that you need to know.  If you have any questions, or you don't receive that email, please send us an email at <a href="mailto:tasyouthconvention@gmail.com">tasyouthconvention@gmail.com</a> or contact us on Facebook about your registration.</p>
+          <p>You should receive an email confirming your registration and letting you know all the details about TYC that you need to know.  If you have any questions, or you don't receive that email, please send us an email at <a href="mailto:tasyouthcon@gmail.com">tasyouthcon@gmail.com</a> or contact us on Facebook about your registration.</p>
           {this.props.isGroupRegistration &&
             <p>As you've registered your group, once we've authorised it, people in your youth group should be able to register for TYC.</p>
           }
