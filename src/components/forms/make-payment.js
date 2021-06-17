d58fb97603259094beee8beb81b99846e1d2f926
Bratalix <bratalix@gmail.com>
2021-06-17 14:42:32 +1000

    Adding check for free registrations

--- src/components/forms/make-payment.js
+++ src/components/forms/make-payment.js
@@ -5,12 +5,14 @@ import PaypalCheckout from '../PaypalCheckout';
 
 export default ({ paymentAuthorised, price }) => {
   return (
-    <div>
+    <section>
+    {price > 0? <div>
       <h3>Make a Payment to TYC</h3>
       <p>To pay your registration amount of {accounting.formatMoney(price)}, please click the Paypal button below.  After you have entered your paypal information, please leave the window open until confirmation has been received.</p>
       <PaypalCheckout
           paymentAuthorised={paymentAuthorised}
           price={price} />
-    </div>
+    </div> : <div><p>Thank you for your registration.</p></div>}
+    </section>
   )
 }
