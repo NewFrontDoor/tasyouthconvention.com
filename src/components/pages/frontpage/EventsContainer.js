4a62b8b48043be9d73565f3e8de2060df8ba655a
Bratalix <bratalix@gmail.com>
2022-02-01 10:48:02 +1100

    Requested changes

--- src/components/pages/frontpage/EventsContainer.js
+++ src/components/pages/frontpage/EventsContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import EventItem from './models/event'
 import tycsplash from './Background.jpg';
 
@@ -8,10 +8,10 @@ export default () => (
     <div className="header-text text-center">
       <h1 className="pddn-50-top pddn-30-btm white" style={{marginTop: '0px'}}>Annual TYC Events</h1>
       <div id="events" className="row">
-        <EventItem title='Roadshow South' path='#roadshow-south' date="Date TBA" location="Hobart" registrationsOpen={false}  />
-        <EventItem title='Roadshow North' path='#roadshow-north' date="Date TBA" location="Launceston" registrationsOpen={false}  />
+        <EventItem title='Roadshow North' path='#roadshow-north' date="1st April 2022" location="Launceston" registrationsOpen={false}  />
+        <EventItem title='Roadshow South' path='#roadshow-south' date="8th April 2022" location="Hobart" registrationsOpen={false}  />
         <EventItem title='TYLT' path='tylt' date="19th June 2021" location="Campbell Town" registrationsOpen={false}  />
-        <EventItem title='TYC Weekend' path='tyc-weekend' date="8th-10th October 2021" location="Bicheno" registrationsOpen={true}  />  
+        <EventItem title='TYC Weekend' path='tyc-weekend' date="8th-10th October 2021" location="Bicheno" registrationsOpen={false}  />  
       </div>
       </div>
     <div className="arrow text-center"> <i className="fa fa-angle-down fa-4x"></i> </div>
--- src/components/pages/tyc-weekend/Header.js
+++ src/components/pages/tyc-weekend/Header.js
@@ -6,7 +6,7 @@ export default () => (
     <div className="header-text white text-center text-uppercase">
       <h1 className="titlebg pddn-20-top pddn-20-btm">TYC Weekend 2021</h1>
       {/*<h2>Dead or Alive</h2>*/}
-      <h2></h2>
+      <h2> </h2>
       <h3 className="mrgn-50-top white sm-title"><span className="date">8th-10th October<br/>Bicheno, Tasmania</span></h3>
     </div>
     <div className="arrow text-center"> <i className="fa fa-angle-down fa-4x"></i> </div>
